4db6298a414c9f11d588210e3d33367cedae97d9
ardik1999 <74649107+ardik1999@users.noreply.github.com>
2021-09-18 23:09:55 +0700

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoPath  = "mongo_url"
+const mongoPath  = "mongodb+srv://ardik712:ardik712@cluster0.sn2jy.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
